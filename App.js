c4f0b0493f65d823186fcb004cf1b5d4ea981ee8
Mustafa Gangardiwala <mustafagangardiwala@Mustafas-MacBook-Air.local>
2023-06-07 02:07:31 -0400

    Updated all 3 screens

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from "react"
-import { View, StyleSheet } from "react-native"
-import CurrentWeather from "./src/components/CurrentWeather"
-import UpcomingWeather from "./src/components/UpccomingWeather"
+import React, {useState, useEffect} from 'react'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import { useGetWeather } from './hooks/useGetWeather'
 
-const App = () =>{
+
+
+const App = () => {
+
+  const [loading, error, weather] = useGetWeather()
+
+
+  if (weather && weather.list && !loading) {
+    return (
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      {/* <CurrentWeather/>  */}
-      <UpcomingWeather/>
+      {error ? (
+        <ErrorItem />
+      ) : (
+        <ActivityIndicator size={'large'} color={'blue'} />
+      )}
     </View>
   )
 }
+
 const styles = StyleSheet.create({
   container:{
+    justifyContent:"center",
     flex:1
   }
 })
+
 export default App
---
+++ src/demonstration/Counter.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+  const [newCount, setNewCount] = useState(0)
+    useEffect(() =>{
+        console.log(`Our count changed`)
+    },[count])
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{`count: ${count}`}</Text>
+      <Button
+        color={'red'}
+        title={'Increase the'}
+        onPress={() => {
+            setCount(count+1)
+        }}   
+      />
+      <Button color={'green'} title={'Decrease the'} onPress={() => setCount(count -1)} />
+      <Button
+        color={'red'}
+        title={'Increase the'}
+        onPress={() => {
+            setNewCount(count+1)
+        }}   
+      />
+      <Button color={'green'} title={'Decrease the'} onPress={() => setNewCount(count -1)} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange'
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 25,
+    marginTop: 25
+  }
+})
+
+export default Counter
